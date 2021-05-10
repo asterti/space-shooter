fd305b1e3ddf090be416b567659b8366bbe4b2dd
asterti <83963466+asterti@users.noreply.github.com>
2021-05-10 10:18:26 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "Wdb{9F4_6*C/:Um,b@U#":
-            case "girl":return img`
-. . . . . . . . . . . . . . 
-. f f f . f f f f . f f f . 
-f f f f f c c c c f f f f f 
-f f f f b c c c c b f f f f 
-f f f c 3 c c c c 3 c f f f 
-. f 3 3 c c c c c c 3 3 f . 
-. f c c c c c c c c c f f . 
-. f f c c c c c c c c f f . 
-. f f c c c c c c f f f f . 
-. f f f f f f f f f f f f . 
-. . f f f f f f f f f f . . 
-. . e f f f f f f f f e . . 
-. . e f f f f f f f f 4 e . 
-. . 4 f 3 3 3 3 3 e d d 4 . 
-. . e f f f f f f e e 4 . . 
-. . . f f f . . . . . . . . 
-`;
             case "D9-`T_l?fAeyK*B}I;lD":
             case "bullet":return img`
 . . . . . . . . . . . . . . . . 
@@ -40,6 +21,44 @@ f f f c 3 c c c c 3 c f f f
 . . . . . . 2 1 2 . . . . . . . 
 . . . . . . 2 1 2 . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "Wdb{9F4_6*C/:Um,b@U#":
+            case "girl":return img`
+. f f f . f f f f . f f f . 
+f f f f f c c c c f f f f f 
+f f f f b c c c c b f f f f 
+f f f c 3 c c c c 3 c f f f 
+. f 3 3 c c c c c c 3 3 f . 
+. f c c c c c c c c c c f . 
+. f f c c c c c c c c f f . 
+. f f f c c c c c c f f f . 
+. f f f f f f f f f f f f . 
+. . f f f f f f f f f f . . 
+. . e f f f f f f f f e . . 
+. e 4 f f f f f f f f 4 e . 
+. 4 d f 3 3 3 3 3 3 c d 4 . 
+. 4 4 f 6 6 6 6 6 6 f 4 4 . 
+. . . . f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`;
+            case "$j%%dW:s2s~HZ?q?_=y@":
+            case "asteroid":return img`
+. . . . . c c b b b . . . . . . 
+. . . . c b d d d d b . . . . . 
+. . . . c d d d d d d b b . . . 
+. . . . c d d d d d d d d b . . 
+. . . c b b d d d d d d d b . . 
+. . . c b b d d d d d d d b . . 
+. c c c c b b b b d d d b b b . 
+. c d d b c b b b b b b b b d b 
+c b b d d d b b b b b d d b d b 
+c c b b d d d d d d d b b b d c 
+c b c c c b b b b b b b d d c c 
+c c b b c c c c b d d d b c c b 
+. c c c c c c c c c c c b b b b 
+. . c c c c c b b b b b b b c . 
+. . . . . . c c b b b b c c . . 
+. . . . . . . . c c c c . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`bullet`, player_girl, 0, -66)
     projectile.setPosition(player_girl.x, player_girl.y + -10)
+    pause(500)
 })
 let projectile: Sprite = null
+let astroid: Sprite = null
 let player_girl: Sprite = null
+game.showLongText("Space Shooter", DialogLayout.Center)
+game.setDialogTextColor(1)
 player_girl = sprites.create(assets.image`girl`, SpriteKind.Player)
+player_girl.setPosition(80, 105)
 controller.moveSprite(player_girl)
+while (true) {
+    for (let index = 0; index < 10; index++) {
+        astroid = sprites.create(assets.image`asteroid`, SpriteKind.Enemy)
+        astroid.setPosition(randint(0, 99), 5)
+        astroid.setVelocity(0, 35)
+        if (true) {
+        	
+        }
+    }
+    pause(5000)
+}
