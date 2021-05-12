15e4a2cad2e65de53b5ee4feaedb815dc5be38a5
asterti <83963466+asterti@users.noreply.github.com>
2021-05-12 09:51:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,40 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(assets.image`bullet`, player_girl, 0, -66)
+    projetile()
     projectile.setPosition(player_girl.x, player_girl.y + -10)
     pause(500)
+})
+function projetile () {
+    projectile = sprites.createProjectileFromSprite(assets.image`bullet`, player_girl, 0, -66)
+}
+function overlap () {
+    while (astroid.overlapsWith(astroid)) {
+        astroid.setPosition(randint(0, 99), 5)
+    }
+    astroid.setVelocity(0, 35)
+    pause(200)
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    astroid.destroy(effects.fire, 100)
+    projectile.destroy(effects.spray, 100)
+    info.changeScoreBy(1)
 })
 let projectile: Sprite = null
 let astroid: Sprite = null
 let player_girl: Sprite = null
 game.showLongText("Space Shooter", DialogLayout.Center)
-game.setDialogTextColor(1)
 player_girl = sprites.create(assets.image`girl`, SpriteKind.Player)
+projetile()
+player_girl.setStayInScreen(true)
 player_girl.setPosition(80, 105)
 controller.moveSprite(player_girl)
 while (true) {
     for (let index = 0; index < 10; index++) {
         astroid = sprites.create(assets.image`asteroid`, SpriteKind.Enemy)
-        astroid.setPosition(randint(0, 99), 5)
-        astroid.setVelocity(0, 35)
-        if (true) {
-        	
+        astroid.setFlag(SpriteFlag.DestroyOnWall, true)
+        for (let index = 0; index < 1; index++) {
+            astroid.setPosition(randint(0, 99), 5)
+            overlap()
         }
     }
-    pause(5000)
+    pause(7000)
 }
